832998d5e37634ec913e1515d80df718758e3cd6
Adnane-cloud <elbayaz.a020@ucd.ac.ma>
2026-04-02 22:15:02 +0100

    Retrying again

--- app/views/calendar/index.php
+++ app/views/calendar/index.php
@@ -1,4 +1,6 @@
-<?php require __DIR__ . '/../admin/layout/header.php'; ?>
+<?php
+die("The server reached this file successfully!"); 
+require __DIR__ . '/../admin/layout/header.php'; ?>
 
 <!-- FullCalendar CSS -->
 <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css' rel='stylesheet' />
